9cce4a654b347962078636ea0daeba9bf1eabe15
Kemilab <lukasalaj9@gmail.com>
2024-03-04 22:01:47 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -50,18 +50,16 @@ class MainActivity : AppCompatActivity() {
                 recreate()
                 return true
             }
+            R.id.analytics ->{
+                // TODO:
+            }
         }
         return super.onOptionsItemSelected(item)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main)
-        //Toast.makeText(applicationContext, "onCreate", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onCreate")
-        Log.d("My Log D", "onCreate")
-        Log.v("My Log V", "onCreate")
-        Log.w("My Log V", "onCreate")
-        Log.e("My Log V", "onCreate")*/
         sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
         // Restore
         brojac = sharedPreferences.getInt(STEPS, 0)
@@ -82,9 +80,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onCreateContextMenu(menu, v, menuInfo)
         menuInflater.inflate(R.menu.menu_float, menu)
-        if (menu != null) {
-            menu.setHeaderTitle(getString(R.string.choose))
-        }
+        menu?.setHeaderTitle(getString(R.string.choose))
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
@@ -105,70 +101,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        //Toast.makeText(applicationContext, "onStart", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onStart")
-        Log.d("My Log D", "onStart")
-        Log.v("My Log V", "onStart")
-        Log.w("My Log V", "onStart")
-        Log.e("My Log V", "onStart")*/
-    }
-
-    override fun onResume() {
-        super.onResume()
-        //Toast.makeText(applicationContext, "onResume", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onResume")
-        Log.d("My Log D", "onResume")
-        Log.v("My Log V", "onResume")
-        Log.w("My Log V", "onResume")
-        Log.e("My Log V", "onResum")*/
-    }
-
     override fun onPause() {
         super.onPause()
-        //Toast.makeText(applicationContext, "onPause", Toast.LENGTH_SHORT).show()
         // brojac u sharedPreferences
         val textView = findViewById<TextView>(R.id.textViewCounter)
         val brojacValue = textView.text.toString().toIntOrNull() ?: 0
         val editor = sharedPreferences.edit()
         editor.putInt(STEPS, brojacValue)
         editor.apply()
-        /*Log.i("MyLog", "onPause")
-        Log.d("My Log D", "onPause")
-        Log.v("My Log V", "onPause")
-        Log.w("My Log V", "onPause")
-        Log.e("My Log V", "onPause")*/
-    }
-
-    override fun onStop() {
-        super.onStop()
-        //Toast.makeText(applicationContext, "onStop", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onStop")
-        Log.d("My Log D", "onStop")
-        Log.v("My Log V", "onStop")
-        Log.w("My Log V", "onStop")
-        Log.e("My Log V", "onStop")*/
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        //Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onRestart")
-        Log.d("My Log D", "onRestart")
-        Log.v("My Log V", "onRestart")
-        Log.w("My Log V", "onRestart")
-        Log.e("My Log V", "onRestart")*/
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        //Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
-        /*Log.i("MyLog", "onDestroy")
-        Log.d("My Log D", "onDestroy")
-        Log.v("My Log V", "onDestroy")
-        Log.w("My Log V", "onDestroy")
-        Log.e("My Log V", "onDestroy")*/
     }
 
     fun setOnClickListenerUp(view: View) {
@@ -194,7 +134,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, getString(R.string.error), Toast.LENGTH_SHORT).show()
         }
     }
-
 
     //languge
     @Suppress("DEPRECATION")
