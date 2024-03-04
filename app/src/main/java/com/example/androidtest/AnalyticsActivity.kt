1b755f22243a4f9dbfa44e50770a174a3d1e2972
Lovro05 <105358917+Lovro05@users.noreply.github.com>
2024-03-04 20:13:37 +0100

    Analytics design

---
+++ app/src/main/java/com/example/androidtest/AnalyticsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnalyticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analytics)
+    }
+}
